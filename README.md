d3ef8531b227a1b1d6f68aec3219d51044bb4c5a
we.sy0x26ler <salcides741@gmail.com>
2025-10-17 11:25:31 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portifolio0x26
+
---
+++ script.js
@@ -0,0 +1,13 @@
+const btnBloqued = document.querySelector('.btnBloqued')
+btnBloqued.addEventListener("click", () =>{
+    alert('Olá, Aqui é o Syler. Desativei os botões. Vou liberá-los no dia do lançamento. \n Hi,This is Syler. I disabled the buttons. I will release them on launch day.')
+})
+const btnsBlo = document.querySelectorAll('.bloqued');
+
+/* btnsBlo.forEach(btn => {
+  btn.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    alert('Olá, Aqui é o Syler. Desativei os botões. Vou liberá-los no dia do lançamento. \nHi, This is Syler. I disabled the buttons. I will release them on launch day.');
+  });
+});
+ */
