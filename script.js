1e256d115874893b2a35671ee558f82aa3898bc6
we.sy0x26ler <salcides741@gmail.com>
2025-10-21 09:31:01 -0300

    add

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const btnBloqued = document.querySelector('.btnBloqued')
-btnBloqued.addEventListener("click", () =>{
-    alert('Olá, Aqui é o Syler. Desativei os botões. Vou liberá-los no dia do lançamento. \n Hi,This is Syler. I disabled the buttons. I will release them on launch day.')
+btnBloqued.addEventListener("click", () => {
+  alert('Olá, Aqui é o Syler. Desativei os botões. Vou liberá-los no dia do lançamento. \n Hi,This is Syler. I disabled the buttons. I will release them on launch day.')
 })
 const btnsBlo = document.querySelectorAll('.bloqued');
 
-/* btnsBlo.forEach(btn => {
-  btn.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    alert('Olá, Aqui é o Syler. Desativei os botões. Vou liberá-los no dia do lançamento. \nHi, This is Syler. I disabled the buttons. I will release them on launch day.');
-  });
-});
- */
+function Theme() {
+  document.body.classList.toggle('is-light')
+  document.body.classList.toggle('is-dark')
+}
+document.querySelector('#trocarDeTema').addEventListener('click', Theme)
